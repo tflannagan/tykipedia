0c1218ee0690e297892664bb02f8e332aa09281a
Ty Flannagan <tylerflannagan@yahoo.com>
2023-03-17 22:03:48 -0700

    initial commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,12 @@
+export default function Index() {
+  return (
+    <p id="zero-state">
+      This is a wiki made just for me.
+      <br />
+      Use this:{" "}
+      <a href="https://docs.google.com/document/d/1OU8YCrh9ef9RTuToCejIY_4d_LZFP3795lCavD4BQUE/edit">
+        Documentation
+      </a>
+    </p>
+  );
+}
